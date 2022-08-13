f244a623277f46ea6cee8d07edcba5f2dca2cfd8
khiz125 <ken.hiro118@gmail.com>
2022-08-13 14:04:23 +0900

    feat: set up for deploy

--- src/components/TemplateMessages.tsx
+++ src/components/TemplateMessages.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import { getFirebaseDb, getFirebaseAuth } from "../firebase";
+import { Firestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
 
 function TemplateMessages() {
+
+  const auth = getFirebaseAuth();
+  const db = getFirebaseDb();
+  const user = auth.currentUser;
+
+  async function sendMessage(event: string) {
+
+    await addDoc(collection(db, "messages"), {
+      text: event,
+      uid: user?.uid,
+      photoURL: user?.photoURL,
+      createdAt: serverTimestamp()
+    })
+  }
   return (
     <>
       <div className='msgContainer'>
         <ul>
-          <li>緊急</li>
-          <li>電話ください</li>
-          <li>無事です</li>
-          <li>外出中</li>
+          <li 
+            onClick={(e) => sendMessage(e.currentTarget.innerText)}
+          >緊急</li>
+          <li 
+            onClick={(e) => sendMessage(e.currentTarget.innerText)}
+          >電話ください</li>
+          <li
+            onClick={(e) => sendMessage(e.currentTarget.innerText)}
+          >無事です</li>
+          <li
+            onClick={(e) => sendMessage(e.currentTarget.innerText)}
+          >外出中</li>
         </ul>
       </div>
     </>
